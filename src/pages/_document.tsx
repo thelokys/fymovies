2043612ed9b5776e30f50b3e3c1bf93d9b785c1a
Thelokys <ricardo.guidetti@hotmail.com>
2022-08-14 14:17:45 -0300

    feat: changed favicon and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,10 @@ export default class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,700&display=swap" rel="stylesheet" />
             
-        <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/png"/>
       </Head>
       <body>
+        <title>FyMovies</title>
         <Main />
         <NextScript />
       </body>
